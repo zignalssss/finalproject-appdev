c3fb58352e0d26a7ca37c4e05c62367b8446076f
zjeee <wiritipon.t@ku.th>
2024-01-24 15:17:40 +0700

    test-api-express

--- server/App.js
+++ server/App.js
@@ -4,6 +4,13 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/test', (req, res) => {
+  res.json({ 
+    number: 1, 
+    name: 'John', 
+    gender: 'male'
+  }); 
+});
 app.listen(5050, () => {
     console.log('sever start successfuly!')
 })
