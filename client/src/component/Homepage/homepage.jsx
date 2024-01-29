c5dda544bff16bc4a49ec2a00ada579bda4bd0a2
zjeee <wiritipon.t@ku.th>
2024-01-30 01:59:45 +0700

    v2

--- client/src/component/Homepage/homepage.jsx
+++ client/src/component/Homepage/homepage.jsx
@@ -1,14 +1,26 @@
-
 export default function homepage(){
     return(
-        <div className = "h-fulll h-screen bg-no-repeat bg-cover opacity-75 bg-homepage-img-1 flex justify-center items-center">
-            <div className=" text-white font-kanit">
-                <div className=" flex flex-col text-center gap-y-5">
-                    <h2 className="text-3xl font-medium">แพลตฟอร์มคอร์สเรียนออนไลน์</h2>
-                    <h1 className="text-5xl font-bold">&lt; Online Course for Computer Engineering /&gt;</h1>
-                    <h2 className="text-3xl font-medium">อัพสกิลความรู้เพื่อคุณโดยเฉพาะ</h2>
+        <>
+            <div className = "h-fulll h-screen bg-no-repeat bg-cover bg-homepage-img-1 flex justify-center items-center">
+                    <div className=" text-white font-kanit">
+                        <div className=" flex flex-col text-center gap-y-5">
+                            <h2 className="text-3xl font-medium">แพลตฟอร์มคอร์สเรียนออนไลน์</h2>
+                            <h1 className="text-5xl font-bold">&lt; Online Course for Computer Engineering /&gt;</h1>
+                            <h2 className="text-3xl font-medium">อัพสกิลความรู้เพื่อคุณโดยเฉพาะ</h2>
+                        </div>
+                    </div>
+            </div>
+            <div>
+                <div className = "h-fulll h-screen bg-no-repeat bg-cover bg-homepage-img-2 flex justify-center items-center">
+                        <div className=" text-white font-kanit">
+                            <div className=" flex flex-col text-center gap-y-5">
+                                <h2 className="text-3xl font-medium">แพลตฟอร์มคอร์สเรียนออนไลน์</h2>
+                                <h1 className="text-5xl font-bold">&lt; Online Course for Computer Engineering /&gt;</h1>
+                                <h2 className="text-3xl font-medium">อัพสกิลความรู้เพื่อคุณโดยเฉพาะ</h2>
+                            </div>
+                        </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
