9c26a133e38f28617f78a538c2d8b2c64beba87b
zjeee <wiritipon.t@ku.th>
2024-02-02 19:31:30 +0700

    hompage_newcourse

--- client/src/component/Homepage/homepage.jsx
+++ client/src/component/Homepage/homepage.jsx
@@ -4,11 +4,27 @@ import 'aos/dist/aos.css';
 import { BiLogoGmail } from "react-icons/bi";
 import { FaPhoneSquareAlt } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
+import Card from "../course/card";
 
 export default function homepage(){
     useEffect(() =>{
         AOS.init({duration: 500})
     },[]);
+    const course_1 = {  altname:"algorithm",
+                        course:"Algorithm Design And Analysis",
+                        pg:"เป็นวิชาที่จะสอนใช้ Algorithm เพื่อเเก้ปัญหาต่างๆ",
+                        img:"https://cdn.discordapp.com/attachments/744501598230937620/1202948423297269801/ales-nesetril-Im7lZjxeLhg-unsplash.jpg?ex=65cf5005&is=65bcdb05&hm=953233f15b30b8dc443a2ef703487463b4bc89428ec115fa8e2a32c769700304&"
+    }
+    const course_2 = {  altname:"absdata",
+                        course:"Abstract Data Type and Problem Solving",
+                        pg:"เป็นวิชาที่จะสอนเกี่ยวกับ Data Structure ต่างๆ",
+                        img:"https://cdn.discordapp.com/attachments/744501598230937620/1202948423297269801/ales-nesetril-Im7lZjxeLhg-unsplash.jpg?ex=65cf5005&is=65bcdb05&hm=953233f15b30b8dc443a2ef703487463b4bc89428ec115fa8e2a32c769700304&"
+    }
+    const course_3 = {  altname:"discrete",
+                        course:"Discrete Mathematics",
+                        pg:"เป็นการศึกษาโครงสร้างทางคณิตศาสตร์ซึ่งมีลักษณะเป็นค่าเฉพาะเจาะจง และไม่ต่อเนื่อง",
+                        img:"https://cdn.discordapp.com/attachments/744501598230937620/1202948423297269801/ales-nesetril-Im7lZjxeLhg-unsplash.jpg?ex=65cf5005&is=65bcdb05&hm=953233f15b30b8dc443a2ef703487463b4bc89428ec115fa8e2a32c769700304&"
+    }
     return(
         <>
             <div className = "h-fulll h-screen bg-no-repeat bg-cover bg-fixed bg-homepage-img-1 flex justify-center items-center">
@@ -54,10 +70,17 @@ export default function homepage(){
             </div>
             <div className = "h-fulll h-screen bg-no-repeat bg-cover bg-fixed bg-homepage-img-1 flex justify-center items-center">
                     <div className=" text-white font-kanit" data-aos="fade-up">
-                        <div className=" flex flex-col text-center gap-y-5">
-                            <h2 className="text-1xl font-medium">แพลตฟอร์มคอร์สเรียนออนไลน์</h2>
-                            <h1 className="text-3xl font-bold">&lt; Online Course for Computer Engineering /&gt;</h1>
-                            <h2 className="text-1xl font-medium">อัพสกิลความรู้เพื่อคุณโดยเฉพาะ</h2>
+                        <div>
+                            <div className="m-10  ">
+                                <h1 className="underline-offset-8 text-bold text-2xl">
+                                    New Course
+                                </h1>
+                            </div>
+                            <div className="flex flex-row gap-x-5">
+                                <Card {...course_1}/>
+                                <Card {...course_2}/>
+                                <Card {...course_3}/>
+                            </div>
                         </div>
                     </div>
             </div>
