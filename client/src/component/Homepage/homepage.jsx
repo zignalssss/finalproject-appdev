dd5f7b77c277e79fd30c40164ce57e48a664eb81
zjeee <wiritipon.t@ku.th>
2024-02-03 16:10:22 +0700

    delete

--- client/src/component/Homepage/homepage.jsx
+++ client/src/component/Homepage/homepage.jsx
@@ -4,12 +4,8 @@ import "aos/dist/aos.css";
 import Firstpage from "./firstpage";
 import ContactUS from "./contactus";
 import Newcourse from "./Newcourse";
-import Card from "../course/card";
 import Navbar from "../Navbar/navbar"
 export default function Homepage() {
-    useEffect(() => {
-        AOS.init({ duration: 500 });
-      }, []);
   return (
     <>
         <Navbar/>
