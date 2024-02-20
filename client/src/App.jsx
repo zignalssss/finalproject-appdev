5eb25faa9f03761192d3d54a5b8b1a307797f456
Peerapat <pplike212121@gmail.com>
2024-02-21 02:10:29 +0700

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Route,Routes } from "react-router-dom";
 import Homepage from "./component/Homepage/Homepage";
 import Signup from "./component/signup/RegisterPage"
+import LoginPage from "./component/Login/LoginPage";
 export default function App() {
     return(
         <>
             <Routes>
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="/signup" element={<Signup/>}/>
-                {/* <Route path="/signin" element={<Homepage/>}/> */}
+                <Route path="/login" element={<LoginPage/>}/>
                 
 
             </Routes>
