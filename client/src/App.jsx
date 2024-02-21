a4ecc35ae552f955f2f61f204ef8f87979bb6e21
zjeee <wiritipon.t@ku.th>
2024-02-22 02:05:14 +0700

    add learning path component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
 import { Route,Routes } from "react-router-dom";
 import Homepage from "./component/Homepage/Homepage";
 import Signup from "./component/signup/RegisterPage"
-import LoginPage from "./component/Login/LoginPage";
-import RegisterPage from "./component/signup/RegisterPage";
+import SigninPage from "./component/Login/LoginPage";
 import AddCourse from "./adminpage/addCourse";
+import LearningPath from "./component/learningPath/LearningPath";
 export default function App() {
     return(
         <>
             <Routes>
                 <Route path="/" element={<Homepage/>}/>
                 <Route path="/signup" element={<Signup/>}/>
-                <Route path="/signin" element={<LoginPage/>}/>
-
+                <Route path="/signin" element={<SigninPage/>}/>
+                <Route path="/webpath" element={<LearningPath/>}/>
                 
                 <Route path="/admin/addcourse" element={<AddCourse/>}></Route>
             </Routes>
--- client/src/component/Navbar/navbar.jsx
+++ client/src/component/Navbar/navbar.jsx
@@ -11,14 +11,9 @@ export default function navbar(){
     return (
         <div className="text-white p-1 font-inter bg-[#18181b] sticky top-0 z-[10]">
             <div>    
-                <ul className=" list-none flex justify-end items-center">
+                <ul className=" list-none flex justify-end items-center m-4">
                     <li className="text-sm/[5px] pl-3 "><ButtonLink to = "/signup">SignUp</ButtonLink></li>
                     <li className="text-sm/[5px] pr-3 pl-3"><ButtonLink to = "/signin">SignIn</ButtonLink></li>
-                    <div className="avatar">
-                        <div className="w-10 mr-5 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                            <img src="https://cdn.discordapp.com/attachments/928273696588726312/1203384660923514940/user.png?ex=65d0e64c&is=65be714c&hm=96a20824110bffc52d6291502a2c24243f7b55f4b52452810f136f6289245e63&" />
-                        </div>
-                    </div>
                 </ul>
             </div>
         </div>
