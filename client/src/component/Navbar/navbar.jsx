1969ea8d8a764c17a8f8749a69aa8cc998b6bd97
zjeee <wiritipon.t@ku.th>
2024-02-24 16:20:54 +0700

    no avatart

--- client/src/component/Navbar/navbar.jsx
+++ client/src/component/Navbar/navbar.jsx
@@ -10,12 +10,12 @@ export default function navbar(){
     },[]);
     return (
         <div className="text-white p-1 font-inter bg-[#18181b] sticky top-0 z-[10]">
-            <div>    
-                <ul className=" list-none flex justify-end items-center">
-                    <li className="text-sm/[5px] pl-3 "><ButtonLink to = "/signup">SignUp</ButtonLink></li>
-                    <li className="text-sm/[5px] pr-3 pl-3"><ButtonLink to = "/signin">SignIn</ButtonLink></li>
-                </ul>
-            </div>
+        <div>    
+            <ul className=" list-none flex justify-end items-center m-4">
+                <li className="text-sm/[5px] pl-3 "><ButtonLink to = "/signup">SignUp</ButtonLink></li>
+                <li className="text-sm/[5px] pr-3 pl-3"><ButtonLink to = "/signin">SignIn</ButtonLink></li>
+            </ul>
         </div>
+    </div>
     )
 }
