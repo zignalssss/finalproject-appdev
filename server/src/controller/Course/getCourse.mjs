527e040b5f8afc4f965bee4c84259d382740be2b
zjeee <wiritipon.t@ku.th>
2024-02-28 01:15:23 +0700

    create getcourse api

--- server/src/controller/Course/getCourse.mjs
+++ server/src/controller/Course/getCourse.mjs
@@ -1,14 +1,17 @@
-import { client , db_name} from '../../server.mjs';
-export const addCourse = async(req,res) => {
-    try{
-        await client.connect()
-        await client.db(db_name).collection("courseData").find({}).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
-        res.status(200).send({message:"Insert Successfully!"});
-    }catch(e){
+import { client, db_name } from '../../server.mjs';
+
+export const getCourse = async (req, res) => {
+    try {
+        await client.connect();
+
+        const database = client.db(db_name);
+        const collection = database.collection("courseData");
+
+        const data = await collection.find().toArray();
+
+        res.json(data);
+    } catch (e) {
         console.log(e);
-        res.status(404).send({message:"Insert Error!"});
+        // res.status(404).send({ message: "Error occurred while connecting to the database!" });
     }
-}
+};
--- server/src/controller/User/register.mjs
+++ server/src/controller/User/register.mjs
@@ -1,9 +1,11 @@
 import { client , db_name} from '../../server.mjs';
+import { v4 as uuidv4 } from 'uuid';
 import { hashPassword } from '../../utils/ManagePassword.mjs';
 export const register = async(req,res) =>{
     try{
         const {username,password,gmail,fname,lname,phonenumber} = req.body;
         const data = {
+                    _id:await uuidv4(),
                     username,
                     password:await hashPassword(password),
                     gmail,
