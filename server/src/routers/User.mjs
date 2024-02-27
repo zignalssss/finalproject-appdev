8f2e7f820972168ac24c900cd6689b968baf0574
zjeee <wiritipon.t@ku.th>
2024-02-28 04:14:20 +0700

    update point api and navbar after login

--- server/src/routers/User.mjs
+++ server/src/routers/User.mjs
@@ -1,10 +1,12 @@
 import express from "express";
 import { register } from "../controller/User/register.mjs"
 import { login } from "../controller/User/login.mjs"
+import { updatePoint } from "../controller/User/updatePoint.mjs";}
 
 const router = express.Router(); 
 
 router.post("/register",register);
 router.post("/login",login);
+router.put("/updatepoint",updatePoint)
 
 export default router;
