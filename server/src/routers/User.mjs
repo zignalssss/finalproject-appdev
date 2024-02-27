50a99496b33ac7fbc4b072cd339773946d82e416
zjeee <wiritipon.t@ku.th>
2024-02-28 04:22:26 +0700

    usercourse take

--- server/src/routers/User.mjs
+++ server/src/routers/User.mjs
@@ -2,11 +2,12 @@ import express from "express";
 import { register } from "../controller/User/register.mjs"
 import { login } from "../controller/User/login.mjs"
 import { updatePoint } from "../controller/User/updatePoint.mjs";
-
+import { UserCourse } from "../controller/User/adduserCourse.mjs";
 const router = express.Router(); 
 
 router.post("/register",register);
 router.post("/login",login);
+router.post("/adduserCourse",UserCourse)
 router.put("/updatepoint",updatePoint)
 
 export default router;
