979155de34426990c8b3a7c355995b874f75dce3
zjeeee <wiritipon.t@ku.th>
2024-02-29 14:31:57 +0700

    new Nav

--- client/src/component/Homepage/newcourse.jsx
+++ client/src/component/Homepage/newcourse.jsx
@@ -12,21 +12,21 @@ const Newcourse = () =>{
         altname: "algorithm",
         courseName: "Algorithm Design And Analysis",
         pg: "เป็นวิชาที่จะสอนใช้แนวคิดอย่างมีเหตุผลในการอธิบายวิธีการอย่างเป็นขั้นตอน",
-        img: "https://cdn.discordapp.com/attachments/928273696588726312/1203430942774136972/algor.png?ex=65d11167&is=65be9c67&hm=b816851d65e1753f55fb10901fbd861fbde974b92b916f7091f2d6dbe6401248&",
+        img: "https://cdn.discordapp.com/attachments/928273696588726312/1203430942774136972/algor.png?ex=65ecc0e7&is=65da4be7&hm=f4f158f0f9bfbd3d2d7e91c1c2f72a54fd51f63c3697141aaddfffff3a31d9d8&",
         badge:"Algorithm"
       };
       const course_2 = {
         altname: "absdata",
         courseName: "Abstract Data Type",
         pg: "เป็นวิชาที่จะสอนวิธีการจัดเก็บข้อมูลในคอมพิวเตอร์เพื่อให้สามารถใช้งานได้อย่างมีประสิทธิภาพ",
-        img: "https://cdn.discordapp.com/attachments/928273696588726312/1203433364560805988/abstract.png?ex=65d113a8&is=65be9ea8&hm=aa95ab93448ea53c525bc5450f751eaa63ec3a94627a5fd0b5cb826203a8b106&",
+        img: "https://cdn.discordapp.com/attachments/928273696588726312/1203433364560805988/abstract.png?ex=65ecc328&is=65da4e28&hm=1bb5e02ba0a46a703989e7442d362a5ff862a2952c1b68533c69b62d1d68a92a&",
         badge:"Data Structure"
       };
       const course_3 = {
         altname: "discrete",
         courseName: "Discrete Mathematics",
         pg: "เป็นการศึกษาโครงสร้างทางคณิตศาสตร์ซึ่งมีลักษณะเป็นค่าเฉพาะเจาะจง และไม่ต่อเนื่อง ",
-        img: "https://cdn.discordapp.com/attachments/928273696588726312/1203442322449104967/ales-nesetril-Im7lZjxeLhg-unsplash_1.png?ex=65d11c00&is=65bea700&hm=4a08a393f67113b6dcfffb3258d03273166033d4495303c40007dd6cffb48989&",
+        img: "https://cdn.discordapp.com/attachments/928273696588726312/1203442322449104967/ales-nesetril-Im7lZjxeLhg-unsplash_1.png?ex=65eccb80&is=65da5680&hm=ad3097e8ada53f71fa41b49dec2ff3cb2c981df333ba8abd743c149035cd7c37&",
         badge:"Math"
       };
     return(
--- client/src/component/Navbar/afternavbar.jsx
+++ client/src/component/Navbar/afternavbar.jsx
@@ -1,7 +1,36 @@
-export default function AfterNav (){
-    return(
-        <>
-        
-        </>
+import { useEffect } from "react"
+import { FaUserCircle } from "react-icons/fa";
+import ButtonLink from "../buttonLink/ButtonLink"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+export default function AfterNav(){
+    useEffect(() =>{
+        AOS.init({duration: 500})
+    },[]);
+    return (
+        <div className="navbar bg-base-100 text-white">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">Next Future</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li>
+                        <details>
+                            <summary>Learning Path</summary>
+                            <ul className="p-2">
+                            <li><a>Web Application</a></li>
+                            <li><a>Problem Solving</a></li>
+                            </ul>
+                        </details>
+                    </li>
+                    <li><a>Course</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn">Button</a>
+            </div>
+      </div>
     )
 }
