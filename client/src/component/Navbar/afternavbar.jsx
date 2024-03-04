edbdf08015fb25042e0cb07f3adf73d2ffdcf794
zjeee <wiritipon.t@ku.th>
2024-03-05 02:36:37 +0700

    glow text v1

--- client/src/component/Navbar/afternavbar.jsx
+++ client/src/component/Navbar/afternavbar.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react"
 import { axiosInstance } from "../../lib/axios";
-import ButtonLink from "../buttonLink/ButtonLink"
 import { RxAvatar } from "react-icons/rx";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
@@ -26,7 +25,9 @@ export default function AfterNav({userObj}){
         <div className="bg-[#18181b] sticky top-0 z-[10]">
             <div className="navbar bg-base-100 text-white ">
                 <div className="navbar-start">
-                    <Link to="/allcourse"><div className="ml-3 text-xl font-bold bg-gradient-to-r from-[#c8c9d9] via-[#8bcbe4] to-[#abaee9]  text-transparent bg-clip-text ">Next Future</div></Link>
+                    <h1 className="ml-3 bg-gradient-to-r from-[#c8c9d9] via-[#8bcbe4] to-[#abaee9]  text-transparent bg-clip-text  text-xl font-bold text-white drop-shadow-[0_0_10px_rgba(0,255,255,0.7)] hover:drop-shadow-[0_0_20px_rgba(0,0,255,0.6)] transition-all duration-200 hover:text-2xl">
+                        <Link to="/allcourse">Next Future</Link>
+                    </h1>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className=" menu menu-horizontal px-1">
@@ -53,7 +54,7 @@ export default function AfterNav({userObj}){
                         </div>
                         <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-[#18181b] rounded-box w-52 ">
                             <li><a onClick={isClick}>Logout</a></li>
-                            <li><a>Setting</a></li>
+                            <li><Link to="/setting">Setting</Link></li>
                         </ul>
                     </div>
                 </div>
--- client/src/component/SettingPage/SettingPage.jsx
+++ client/src/component/SettingPage/SettingPage.jsx
@@ -1,8 +1,11 @@
+import {Link} from "react-router-dom";
+
 const SettingPage = () =>{
     return(
+
         <div className="space-y-6 h-screen flex justify-center items-center">
             <h1 className="text-3xl font-bold text-white drop-shadow-[0_0_10px_rgba(0,255,255,0.7)] hover:drop-shadow-[0_0_20px_rgba(0,0,255,0.6)] transition-all duration-300 hover:text-4xl">
-                Glowing Shadow Text
+                <Link to="/allcourse">Glowing Shadow Text</Link>
             </h1>
         </div>
     )
