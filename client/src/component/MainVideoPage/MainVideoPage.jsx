6dcd966f9e8bbc791665aa76786d372233e3e76f
zjeeee <wiritipon.t@ku.th>
2024-03-07 14:44:10 +0700

    use data from databases mongodb

---
+++ client/src/component/MainVideoPage/MainVideoPage.jsx
@@ -0,0 +1,9 @@
+import { axiosInstance } from "../../lib/axios"
+import { useEffect,useState } from "react"
+const MainVideoPage = ()=>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
