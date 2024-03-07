3c7d63e4b2ac143e9f6d9213e1b2f434c63b5246
Peerapat <pplike212121@gmail.com>
2024-03-08 03:33:55 +0700

    imagebug

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -14,8 +14,8 @@ export default {
         'homepage-img-1': "url('src/assets/img/bg_img3.jpg')",
         'homepage-img-2': "url('src/assets/img/bg_img1.jpg')",
         'homepage-img-3': "url('src/assets/img/bg_img2.jpg')",
-        'login_img1':"url('src/assets/img/BG_image_blur.jpg')",
-        'login_imge':"url('src/assets/img/BG_image.jpg')",
+        'login_img1':"url('src/assets/img/BG-img-log-reg.png')",
+        'login_imge':"url('src/assets/img/BG-img10.jpg')",
       },
       fontFamily:{
           inter :["Inter", "sans-serif"],
