c126ab128f4fa5b5ad2fcc19defca0c353909fcd
zjeee <wiritipon.t@ku.th>
2024-03-08 01:41:35 +0700

    fix add point V2

--- server/src/controller/User/updatePoint.mjs
+++ server/src/controller/User/updatePoint.mjs
@@ -3,20 +3,31 @@ import { client , db_name} from '../../server.mjs';
 export const updatePoint = async(req,res) => {
     const userId = req.body._id;
     const updatedPoint = req.body.point;
-    await client.connect()
-    const findUser = await client.db(db_name).collection("userData").findOne({_id:userId});
-    const userpoint = findUser.point;
-    const newpoint = updatePoint - userpoint;
-    await client.db(db_name).collection("userData").updateOne(
-        { _id: userId },
-        { $set: { point: newpoint } },
-        (err, result) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Error updating item');
-            } else {
-                res.status(200).send('Item updated successfully');
-            }
+    try{
+        await client.connect()
+        const findUser = await client.db(db_name).collection("userData").findOne({_id:userId});
+        const userpoint = findUser.point;
+        const newpoint = userpoint - updatedPoint  ;
+        if (!findUser) {
+            res.status(401).json({ message: "User not found" });
+            console.log("User not found");
+            return false;
         }
-        );
+        if(newpoint <= 0){
+            await client.db(db_name).collection("userData").updateOne(
+                { _id: userId },
+                { $set: { point: 0 } },
+                );
+            return false;
+        }
+        await client.db(db_name).collection("userData").updateOne(
+            { _id: userId },
+            { $set: { point: newpoint } },
+            );
+        
+        res.send("Update Point Successfuly")
+    }catch(e){
+        console.log(e)
+    }
+  
 };
