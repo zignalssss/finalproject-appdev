552947555d83f3c1a3cd032ae04cff793345d23f
Peerapat <pplike212121@gmail.com>
2024-03-20 23:27:58 +0700

    set component

--- client/src/component/quizpage/Quizpage.jsx
+++ client/src/component/quizpage/Quizpage.jsx
@@ -76,27 +76,27 @@ const quizpage = () =>{
     
       return (
         <>
-        {/* <AfterNav userObj={userInfo} /> */}
+        <AfterNav userObj={userInfo} />
           <div className="flex justify-center items-center w-screen h-screen bg-cover bg-login_imge">
           <form className="bg-slate-700 p-12 rounded-[60px] bg-opacity-40 shadow-5xl border-white border-t-2 border-l-2 border-opacity-20 backdrop-filter w-2/6 h-1/1">
             <div className="flex justify-center text-white text-[50px] font-kanit">QUIZ</div>
-            <div className="flex justify-center text-white text-[40px] font-kanit">text1</div>
+            <div className="flex justify-center text-white text-[40px] font-kanit">{questions[currentQuestion].qText}</div>
             <div className="text-black flex flex-col gap-y-5 items-center mt-6">
                 <button onClick={(e)=>{checkAns(e,1)}} className= 'btn bg-white rounded-ms w-[350px] border-none hover:text-white hover:bg-cyan-500 hover:shadow-cyan-500 hover:text-base/[3px] hover:scale-105 duration-200 hover:shadow-[0_0_10px_rgba(255,255,255,0.7)] '>
-                  {/* {questions[currentQuestion].C1} */}
-                  text2
+                  {questions[currentQuestion].C1}
+                  {/* text2 */}
                 </button>
                 <button onClick={(e)=>{checkAns(e,2)}} className= 'mt-2 btn bg-white rounded-ms w-[350px] border-none hover:text-white hover:bg-cyan-500 hover:shadow-cyan-500 hover:text-base/[5px] hover:scale-105 duration-200 hover:shadow-[0_0_10px_rgba(255,255,255,0.7)]'>
-                  {/* {questions[currentQuestion].C2} */}
-                  Text3
+                  {questions[currentQuestion].C2}
+                  {/* Text3 */}
                 </button>
                 <button onClick={(e)=>{checkAns(e,3)}} className= 'mt-2 btn bg-white rounded-ms w-[350px] border-none hover:text-white hover:bg-cyan-500 hover:shadow-cyan-500 hover:text-base/[5px] hover:scale-105 duration-200 hover:shadow-[0_0_10px_rgba(255,255,255,0.7)]'>
-                  {/* {questions[currentQuestion].C3} */}
-                  Text4
+                  {questions[currentQuestion].C3}
+                  {/* Text4 */}
                 </button>
                 <button onClick={(e)=>{checkAns(e,4)}} className= 'mt-2 btn bg-white rounded-ms w-[350px] border-none hover:text-white hover:bg-cyan-500 hover:shadow-cyan-500 hover:text-base/[5px] hover:scale-105 duration-200 hover:shadow-[0_0_10px_rgba(255,255,255,0.7)]'>
-                  {/* {questions[currentQuestion].C4} */}
-                  Text5
+                  {questions[currentQuestion].C4}
+                  {/* Text5 */}
                 </button>
             </div>
           </form>
