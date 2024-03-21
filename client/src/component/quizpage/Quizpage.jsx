5b5feff5d480c670d4078ee0f5be6ccaea188a9c
zjeee <wiritipon.t@ku.th>
2024-03-21 19:29:05 +0700

    use data from database

--- client/src/component/quizpage/Quizpage.jsx
+++ client/src/component/quizpage/Quizpage.jsx
@@ -66,7 +66,7 @@ const QuizPage = () => {
     try {
       const response = await axiosInstance.get("/api/user/verify")
       const quiz = await axiosInstance.get(`/api/quiz/getquizbyid/${id}`)
-      setQuestions(quiz.data)
+      setQuestions(quiz.data.quiz)
       setUserInfo(response.data);
       setLoading(false);
     } catch (e) {
@@ -91,9 +91,9 @@ const QuizPage = () => {
     <div className="flex justify-center items-center w-screen h-screen bg-cover bg-login_imge">
       <form className="bg-slate-700 p-12 rounded-[60px] bg-opacity-40 shadow-5xl border-white border-t-2 border-l-2 border-opacity-20 backdrop-filter w-2/6 h-1/1">
         <div className="flex justify-center text-white text-[50px] font-kanit">QUIZ</div>
-        <div className="flex justify-center text-white text-[40px] font-kanit">{questions.quiz[currentQuestion].question}</div>
-        {/* <div className="text-black flex flex-col gap-y-5 items-center mt-6">
-          {questions.quiz[currentQuestion].choices.map((choice, index) => (
+        <div className="flex justify-center text-white text-[40px] font-kanit">{questions[currentQuestion].question}</div>
+        <div className="text-black flex flex-col gap-y-5 items-center mt-6">
+          {questions[currentQuestion].choices.map((choice, index) => (
             <button key={index} onClick={(e) => { checkAns(e, choice.answer) }} className='btn bg-white rounded-ms w-[350px] border-none hover:text-white hover:bg-cyan-500 hover:shadow-cyan-500 hover:text-base/[3px] hover:scale-105 duration-200 hover:shadow-[0_0_10px_rgba(255,255,255,0.7)]'>
               {choice.text}
             </button>
@@ -115,7 +115,7 @@ const QuizPage = () => {
             <h1>Sorry, you did not pass the quiz.</h1>
             <button className="btn bg-red-500 text-white rounded-lg px-4 py-2 mt-4" onClick={() => navigate(`/video/${id}`)}>Go Back</button>
           </div>
-        )} */}
+        )}
       </form>
     </div>
     </>
