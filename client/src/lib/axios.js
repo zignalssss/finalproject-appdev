bccade13ff54a1c9b710645412dfd561b95e2c24
zjeee <wiritipon.t@ku.th>
2024-03-28 23:36:29 +0700

    use only env

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL:"http://localhost:5050"  ||   import.meta.env.VITE_REACT_APP_BACKEND,
+  baseURL: import.meta.env.VITE_REACT_APP_BACKEND,
   withCredentials: true,
 });
