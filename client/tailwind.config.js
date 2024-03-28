de15e79522f1fa2c7c2ef25ac36a5a5a399611b3
zjeee <wiritipon.t@ku.th>
2024-03-29 02:35:27 +0700

    using image from firebasestorage

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,11 +11,11 @@ export default {
     
     extend: {
       backgroundImage: {
-        'homepage-img-1': "url('src/assets/img/bg_img3.jpg')",
-        'homepage-img-2': "url('src/assets/img/bg_img1.jpg')",
-        'homepage-img-3': "url('src/assets/img/bg_img2.jpg')",
-        'login_img1':"url('src/assets/img/BG-img-log-reg.png')",
-        'login_imge':"url('src/assets/img/BG-img10.jpg')",
+        'homepage-img-1': "url(https://firebasestorage.googleapis.com/v0/b/appdev-image-storage.appspot.com/o/imgforproject%2Fbg_img3.jpg?alt=media&token=cdcfb637-3ffe-4259-8ff7-12aa4255aa1f)",
+        'homepage-img-2': "url(https://firebasestorage.googleapis.com/v0/b/appdev-image-storage.appspot.com/o/imgforproject%2Fbg_img1.jpg?alt=media&token=898f3d22-e41d-4cc8-b0b5-65cc8257c6e8)",
+        'homepage-img-3': "url(https://firebasestorage.googleapis.com/v0/b/appdev-image-storage.appspot.com/o/imgforproject%2Fbg_img2.jpg?alt=media&token=32f66452-3409-4fcb-8c5a-fba27bfc5b14)",
+        'login_img1':"url(https://firebasestorage.googleapis.com/v0/b/appdev-image-storage.appspot.com/o/imgforproject%2FBG-img-log-reg.png?alt=media&token=c76dd9f5-e9be-4a12-9bbf-60b6072e07eb)",
+        'login_imge':"url(https://firebasestorage.googleapis.com/v0/b/appdev-image-storage.appspot.com/o/imgforproject%2FBG-img10.jpg?alt=media&token=8a94e1c5-9c2f-4284-aeaa-7ad0023b3ed3)",
       },
       fontFamily:{
           inter :["Inter", "sans-serif"],
