ea0948eecc838c019abead7627578ec3dcc86500
zjeee <wiritipon.t@ku.th>
2024-03-29 02:20:56 +0700

    env frontend

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
+import dotenv from 'dotenv';
+dotenv.config({path:'../.env'})
 export const axiosInstance = axios.create({
-  baseURL: "https://nextfuture-api.onrender.com",
+  baseURL: import.meta.env.VITE_REACT_APP_BACKEND,
   withCredentials: true,
 });
