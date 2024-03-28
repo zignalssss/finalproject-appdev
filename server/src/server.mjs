80fd1cfdc1a6d04fef4790932e248f007bfc2a71
zjeee <wiritipon.t@ku.th>
2024-03-29 02:05:41 +0700

    fix deploy ep4

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
 
 app.use(
     cors({
-      origin: "http://localhost:5173" || process.env.origin,
+      origin:process.env.ORIGIN,
       credentials: true,
     })
   );
