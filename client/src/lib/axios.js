6d08bd01e642965b47413dec7b2e4388064beb8a
zjeee <wiritipon.t@ku.th>
2024-03-29 00:29:32 +0700

    try to fix bug ep1

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-import dotenv from 'dotenv';
-dotenv.config({path:'../.env'})
+
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_BACKEND,
+  baseURL: "https://nextfuture-api.onrender.com",
   withCredentials: true,
 });
